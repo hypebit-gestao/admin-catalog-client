4a04ee774ae63d6056cfafea2d4b1bd03ed72e48
rafaelcf00 <rafacesar0070@gmail.com>
2025-02-17 19:29:21 -0300

    updates

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -42,7 +42,7 @@ const Sidebar = () => {
         {session?.user?.user?.id == '18b2e340-ad57-467b-a156-a7f78e6d0ea4' && (
           <MenuItem href="/size" label="Tamanhos" icon={TbRulerMeasure} />
         )}
-       {<MenuItem href="/attribute" label="Atributos" icon={FaCubes} />}
+       {/* {<MenuItem href="/attribute" label="Atributos" icon={FaCubes} />} */}
         <MenuItem href="/order" label="Pedidos" icon={MdRequestPage} />
         <MenuItem
           href="/configurations"
