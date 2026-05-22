e3eafd7e2758f13ca5fd4d636690af9cacd978f1
gestao-hypebit <gestao@hypebit.com.br>
2026-05-22 09:20:13 -0300

    updates

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true,
     domains: [
       "portimages.nyc3.digitaloceanspaces.com",
       "portimages.nyc3.cdn.digitaloceanspaces.com",
       "barber-ai.nyc3.digitaloceanspaces.com",
       "daisyui.com",
       "www.pallenz.co.nz",
+      "assets.olaclick.app",
     ],
   },
 };
